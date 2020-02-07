9c587f694f4275b99a8451fd333a6fda5c7906e3
conbec <56614504+conbec@users.noreply.github.com>
2020-02-06 23:27:12 -0800

    backend working, front end getting data but won't display

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const db = require('./db')
+
+const app = express();
+const PORT = process.env.PORT || 3000
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+app.get('/products', (req, res) => {
+  db.readJSON('./products.json')
+    .then(response => res.send(response))
+    .catch(err => res.send(err))
+});
+
+app.listen(PORT, () => console.log('Listening on ', PORT));
